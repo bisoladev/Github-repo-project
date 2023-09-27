baed1608ca00061b6914161244a44b30bfc3ccb6
Shekinah Adenike Adaramola <bisolaonaolapo81@gmail.com>
2023-09-27 22:56:35 +0100

    Added base route to my project

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Github-repo-project/',
   plugins: [react(), svgr(), tsconfigPaths()],
 });

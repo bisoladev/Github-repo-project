6e5529c6ccb782cc6fbb666682151708176350e8
Shekinah Adenike Adaramola <bisolaonaolapo81@gmail.com>
2023-10-01 17:39:26 +0100

    eslint fix

--- src/Spinner/FullPageSpinner.tsx
+++ src/Spinner/FullPageSpinner.tsx
@@ -1,4 +1,4 @@
-import PageLoadingSpinner from "./PageLoadingSpinner";
+import PageLoadingSpinner from './PageLoadingSpinner';
 
 export default function FullPageSpinner() {
   return <PageLoadingSpinner extraClass="w-full h-full" />;
